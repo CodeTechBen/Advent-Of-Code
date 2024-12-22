2190e8383f9cbefd9ba8fdb1c7a321b210eb7d1f
CodeTechBen <benjamin-smith@live.com>
2024-12-22 10:54:03 +0000

    not solved

--- day-4/main.py
+++ day-4/main.py
@@ -1,24 +1,40 @@
-"""find the count of XMAS in input"""
+def load_input(source):
+    with open(source) as f:
+        data = [x.strip() for x in f.readlines()]
+    return data
 
-def read_input(filepath:str) -> str:
-    """reads the input from a file and returns it"""
-    with open(filepath, "r", encoding="UTF-8") as f:
-        return f.readlines()
-    
-def find_xmas(input:str) -> int:
-    """finds every instance of XMAS and returns the count"""
-    
+
+def find_xmas(grid: list[str]) -> bool:
+    directions = ()  # list of all possible directions
+    pass
+
+
+def find_x(grid: list[str]) -> int:
+    # iterates through the grid to find x then calls find_xmas and counts it
+    pass
+
+
+def main():
+
+    # Example input as multiline string
+    input = """MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX"""
+
+    # Convert input to a list of lists
+    grid = [list(line) for line in input.splitlines()]
+    print(grid)
+
+    # Count occurrences of "XMAS"
+    result = find_x(grid)
+
 
 if __name__ == "__main__":
-    read_input("input.txt")[0][0]
-    input = """MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX""".splitlines()
-
+    main()
