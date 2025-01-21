7e35a9443afb58b56e1d901499129e4e839898f3
CodeTechBen <benjamin-smith@live.com>
2025-01-21 14:06:22 +0000

    solved part 2

--- 2020/day-2/main.py
+++ 2020/day-2/main.py
@@ -26,6 +26,34 @@ def solution_1(passwords: list[str]) -> int:
     return valid_count
 
 
+def solution_2(passwords: list[str]) -> int:
+    """
+    Check the passwords have the letters and amounts of letters required in the right place.
+    Then return the number of passwords that are accepted.
+    """
+
+    valid_passwords = []
+    for password in passwords:
+        # Extract ranges, letter, and password string
+        match = re.match(r"(\d+)-(\d+) (\w): (.+)", password)
+        if match:
+            num1, num2, letter, pwd = match.groups()
+
+            num1, num2 = int(num1), int(num2)
+
+            if (pwd[num1-1] == letter) != (pwd[num2-1] == letter):
+                valid_passwords.append(password)
+
+    return len(valid_passwords)
+
+
+
+
 if __name__ == "__main__":
     passwords = read_data("input.txt")
+
+#     passwords = ['1-3 a: abcde',
+# '1-3 b: cdefg',
+# '2-9 c: ccccccccc']
     print(solution_1(passwords))
+    print(solution_2(passwords))
